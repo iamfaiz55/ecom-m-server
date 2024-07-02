105eacd66c3d6438b1cd0efe055731d265078e21
john <you@example.com>
2024-07-02 19:53:36 +0530

    initial commit"

--- controllers/public.controller.js
+++ controllers/public.controller.js
@@ -2,7 +2,8 @@ const asyncHandler = require("express-async-handler")
 const Product = require("../models/Product")
 
 exports.publicGetAllProducts = asyncHandler(async (req, res) => {
-    const result = await Product.find({ active: true })
+    // const result = await Product.find({ active: true })
+    const result = await Product.find()
     res.json({ message: "Product fetch success", result })
 })
 exports.publicGetProductDetails = asyncHandler(async (req, res) => {
